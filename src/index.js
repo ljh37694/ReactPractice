48c01f3a1f7c67c80b1c75236029a8f1588ff5a8
ljh37694 <ljh37694@naver.com>
2023-12-26 13:44:30 +0900

    Lifting State Up 실습 미완

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import SignUp from './MakeSignUp/SignUp';
+import TemperatureInput from './MakeTemperatureInput/TemperatureInput';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <SignUp />
+    <TemperatureInput />
 );
 
 reportWebVitals();
