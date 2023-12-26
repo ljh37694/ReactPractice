a63c0296979d3be004a74ad6cbe3dc1790a511e4
ljh37694 <ljh37694@naver.com>
2023-12-26 23:06:23 +0900

    Context 실습

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import ProfileCard from './MakeCard/ProfileCard';
+import DarkOrLight from './MakeThemeContext/DarkOrLight';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ProfileCard />
+    <DarkOrLight />
 );
 
 reportWebVitals();
